4e38140fd3bb585c57017254d0362605b1204c2f
Akshat Agrawal <akshat@akshats-mbp.lan>
2022-06-15 16:10:13 -0700

    msg

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log("anon key: " + process.env.REACT_APP_SUPABASE_ANON_KEY)
+console.log("url: " + process.env.REACT_APP_SUPABASE_URL)
+
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
